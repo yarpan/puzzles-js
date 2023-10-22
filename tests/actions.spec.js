987c81e2a2042cf799bbf518267067e585fc7937
Yaroslav Panchishyn <yarpan@gmail.com>
2023-10-22 23:01:16 +0300

    actions added

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -1,20 +1,51 @@
 const {test, expect} = require("@playwright/test");
 
-const uriBase = 'https://the-internet.herokuapp.com/';
-const uriAbTest = 'https://the-internet.herokuapp.com/abtest';
+const uriBase = 'https://the-internet.herokuapp.com';
 
-test('enter text', async ({page}) => {
-    await page.goto(uriBase + 'login');
+test('fill the text and click button', async ({page}) => {
+    await page.goto(uriBase + '/login');
 
     const userName = page.locator('#username');
     await userName.fill('tomsmith',{noWaitAfter:false});
 
-    const UserPass = page.locator('input#password');
-    await userName.type('SuperSecretPassword!',{delay:500});
-
+    const UserPass = page.locator('#password');
+    await UserPass.type('SuperSecretPassword!',{delay:100});
 
     const loginButton = page.locator('button[type="submit"]');
     await loginButton.click();
 
-    expect(page.url()).toBe(uriAbTest);
+    expect(page.url()).toBe(uriBase + '/secure');
+})
+
+
+test('checkbox', async ({page}) => {
+    await page.goto(uriBase + '/checkboxes');
+    const checkbox = page.locator('#checkboxes>input:nth-child(1)');
+
+    await checkbox.check();
+    await checkbox.uncheck();
+
+})
+
+
+test('radiobutton', async ({page}) => {
+    await page.goto('https://www.mortgagecalculator.org');
+    const radio = page.locator('//input[@value="percent"]'); // css = input[value='percent']
+
+    await radio.check();
+
+
+})
+
+
+test('dropdown', async ({page}) => {
+    await page.goto(uriBase + '/dropdown');
+    const dropdown = page.locator('#dropdown'); 
+
+    await dropdown.selectOption("1");
+
+    await dropdown.selectOption({label: "Option 2"});
+
+    await dropdown.selectOption({index: 1});
+
 })
