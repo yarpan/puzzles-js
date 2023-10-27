d3a8402da9e26ca06e9a2c320e80333e5b193db8
Yaroslav Panchishyn <yarpan@gmail.com>
2023-10-27 23:34:16 +0300

    assertions add

--- tests/assertions.spec.js
+++ tests/assertions.spec.js
@@ -8,6 +8,13 @@
 // page have title
 // page have URL
 
+// element has css value
+// element has class
+// count of elements found
+
+//negating assertions
+// soft assertions
+
 
 const {test, expect} = require("@playwright/test");
 
@@ -39,5 +46,44 @@ test('page', async ({page}) => {
     await page.click("a[href='/abtest']");
 
     await expect(page).toHaveURL(uriBase + '/abtest');          // page have URL
+
+})
+
+
+test('advanced asserts', async ({page}) => {
+    await page.goto(uriBase);
+
+    const heading = page.locator('h1.heading');
+    await expect(heading).toHaveCSS('font-style', 'normal');    //assert element has css value
+    
+    const h1 = page.locator('h1');
+    await expect(h1).toHaveClass('heading');                    //assert element has class
+
+    const div = page.locator('div');
+    await expect(div).toHaveCount(7);                           //assert count of elements
+
+})
+
+
+test('reverse assertions', async ({page}) => {
+    await page.goto(uriBase);
+
+    const heading = page.locator('h1.heading');
+    await expect(heading).not. toHaveText('Hello World!');      //assert element has css value
+
+    const div = page.locator('div');
+    await expect(div).not.toHaveCount(6);                       //reverse assertion
+
+})
+
+
+test('soft assertions', async ({page}) => {
+    await page.goto(uriBase);
+
+    const heading = page.locator('h1.heading');
+    await expect.soft(heading, "message for test fail").toHaveText('Hello World!'); //soft assertion
+
+    const div = page.locator('div');
+    await expect.soft(div).toHaveCount(6);                                          //soft assertion
 
 })
