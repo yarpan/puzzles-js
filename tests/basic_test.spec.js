fa27011daafdc452c41da36ad52a2d2ba1ab2b79
Yaroslav Panchishyn <yarpan@gmail.com>
2023-10-28 20:07:51 +0300

    waits add

--- tests/basic_test.spec.js
+++ tests/basic_test.spec.js
@@ -1,4 +1,9 @@
-const {test, expect} = require("@playwright/test")
+const {test, expect} = require("@playwright/test");
+const { chmod } = require("fs");
+
+const uriBase = 'https://the-internet.herokuapp.com/'
+const uriAbTest = 'https://the-internet.herokuapp.com/abtest'
+const locatorAbTest = '#content>ul>li:nth-child(1)>a'
 
 test("test_1", async({page}) =>{
     await page.goto('https://the-internet.herokuapp.com/')
@@ -15,14 +20,33 @@ test("test_2", async({page}) =>{
     expect(page.url()).toBe('https://the-internet.herokuapp.com/abtest')
 })
 
-
-const uriBase = 'https://the-internet.herokuapp.com/'
-const uriAbTest = 'https://the-internet.herokuapp.com/abtest'
-const locatorAbTest = '#content>ul>li:nth-child(1)>a'
-
 test("test_3", async({page}) =>{
     await page.goto(uriBase)
     await page.click(locatorAbTest)
     //const url = await page.url();
     expect(page.url()).toBe(uriAbTest)
+})
+
+
+// run tests from cmd
+// npx playwright test
+// npx playwright show-report
+
+
+//for executing this test only
+//test('fill the text and click button', async ({page}) => {
+test.only('fill the text and click button', async ({page}) => {
+    await page.goto(uriBase + '/login');
+
+    const userName = page.locator('#username');
+    await userName.fill('tomsmith',{noWaitAfter:false});
+
+    const UserPass = page.locator('#password');
+    await UserPass.type('SuperSecretPassword!',{delay:100});
+
+    const loginButton = page.locator('button[type="submit"]');
+    await loginButton.click();
+    //await loginButton.click({force:true});                    //no wait until element actionability
+
+    expect(page.url()).toBe(uriBase + '/secure');
 })
---
+++ tests/waits.spec.js
@@ -0,0 +1,25 @@
+const {test, expect} = require("@playwright/test");
+
+const uriBase = 'https://the-internet.herokuapp.com/';
+const uriAbTest = 'https://the-internet.herokuapp.com/abtest';
+
+test('wait to element state', async ({page}) => {
+    await page.goto(uriBase);
+    const checkbox = page.locator('#checkboxes>input:nth-child(1)');
+    checkbox.waitFor({state:"visible", timeout:1000});
+        //{ state?: "attached" "detached" "visible" "hidden" 
+        //timeout?: number
+    
+})
+
+
+test('wait for network idle', async ({page}) => {
+    await page.goto(uriBase);
+
+    await page.waitForLoadState()
+    // (method) Page.waitForLoadState(state?: "load" | "domcontentloaded" | "networkidle" |
+    //  options?: {   timeout?: number | undefined;
+
+})
+
+
