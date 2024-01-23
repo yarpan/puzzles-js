6cb518fa13b074f09fd1186a316114797e4dbfdf
Yaroslav Panchishyn <yarpan@gmail.com>
2024-01-23 21:48:11 +0200

    refactoring

--- tests/api-reqres.test.js
+++ tests/api-reqres.test.js
@@ -1,19 +1,30 @@
-import { test, expect, APIResponse } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { faker } from '@faker-js/faker';
 
 const BASE_URL = 'https://reqres.in';
 
-
-
 test("get all users", async ({ request }) => {
     const ENDPOINT = '/api/users?page=2';
     const responseRaw = await request.get(BASE_URL + ENDPOINT);
+    const responseJson = await responseRaw.json();
 
     expect(responseRaw.status()).toBe(200);
     expect(responseRaw.statusText()).toBe('OK');
-    const responseJson = await responseRaw.json();
+    expect(responseJson.page).toBe(2);
+    expect(responseJson.per_page).toBe(6);
+    expect(responseJson.total).toBe(12);
+    expect(responseJson.total_pages).toBe(2);
+    expect(responseJson.data.length).toBe(6);
+    expect(responseJson.data).toContainEqual(
+        expect.objectContaining({
+            id: 7,
+            email: "michael.lawson@reqres.in",
+            first_name: "Michael",
+            last_name: "Lawson",
+            avatar: "https://reqres.in/img/faces/7-image.jpg",
+        })
+    );
     console.log(responseJson);
-
 });
 
 test("create new user", async ({ request }) => {
@@ -26,12 +37,16 @@ test("create new user", async ({ request }) => {
             "email": userEmail
         }
     });
+    const responseJson = await responseRaw.json();
 
     expect(responseRaw.status()).toBe(201);
     expect(responseRaw.statusText()).toBe('Created');
-    const responseJson = await responseRaw.json();
+    expect(responseJson.name).toBe("morpheus");
+    expect(responseJson.job).toBe("leader");
+    expect(responseJson.email).toBe(userEmail);
+    expect(responseJson.id).toBeDefined();
+    expect(responseJson.createdAt).toBeDefined();
     console.log(responseJson);
-
 });
 
 test("update user", async ({ request }) => {
@@ -44,89 +59,39 @@ test("update user", async ({ request }) => {
             "email": userEmail
         }
     });
+    const responseJson = await responseRaw.json();
 
     expect(responseRaw.status()).toBe(200);
     expect(responseRaw.statusText()).toBe('OK');
-    const responseJson = await responseRaw.json();
-    console.log('userEmail: ' + userEmail);
+    expect(responseJson.name).toBe("morpheus");
+    expect(responseJson.job).toBe("resident");
+    expect(responseJson.email).toBe(userEmail);
+    expect(responseJson.updatedAt).toBeDefined();
     console.log(responseJson);
-
-
 });
 
 test("patch user", async ({ request }) => {
     const ENDPOINT = '/api/users/2';
-
+    const userEmail = faker.internet.email();
+    const responseRaw = await request.patch(BASE_URL + ENDPOINT, {
+        data: {
+            "job": "member"
+        }
+    });
+    const responseJson = await responseRaw.json();
 
     expect(responseRaw.status()).toBe(200);
     expect(responseRaw.statusText()).toBe('OK');
-    const responseJson = await responseRaw.json();
-
-
+    expect(responseJson.job).toBe("member");
+    expect(responseJson.updatedAt).toBeDefined();
+    console.log(responseJson);
 });
 
 test("delete user", async ({ request }) => {
     const ENDPOINT = '/api/users/2';
+    const responseRaw = await request.delete(BASE_URL + ENDPOINT);
 
-    expect(responseRaw.status()).toBe(200);
-    expect(responseRaw.statusText()).toBe('OK');
-    const responseJson = await responseRaw.json();
-
-
+    expect(responseRaw.status()).toBe(204);
+    expect(responseRaw.statusText()).toBe('No Content');
+    expect(responseRaw.body()).toBeNull;
 });
-
-
-
-
-function createRandomUser() {
-    return {
-        userId: faker.string.numeric(),
-        firstname: faker.person.firstName(),
-        lasttname: faker.person.lastName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
-        job: faker.company.buzzNoun(),
-    };
-
-    // {
-    //     "id": 1,
-    //     "email": "george.bluth@reqres.in",
-    //     "first_name": "George",
-    //     "last_name": "Bluth",
-    //     "avatar": "https://reqres.in/img/faces/1-image.jpg"
-    // },
-
-
-    test("create new user2", async ({ request }) => {
-
-        const responseBody = await response.json();
-        expect(responseBody["id"]).toBeDefined();
-        expect(responseBody["createdAt"]).toBeDefined();
-        expect(responseBody["name"]).toBe("morpheus");
-        expect(responseBody["email"]).toBeDefined();
-        expect(responseBody["email"].includes("@gmail.com")).toBeTruthy();
-    });
-
-    test("get all users2", async ({ request }) => {
-
-        const responseJson = await responseRaw.json();
-
-        expect(responseJson["page"]).toBe(2);
-        expect(responseJson["per_page"]).toBe(6);
-        expect(responseJson["total"]).toBe(12);
-        expect(responseJson["total_pages"]).toBe(2);
-        expect(responseJson["data"].length).toBe(6);
-
-        expect(responseJson["data"]).toContainEqual(
-            expect.objectContaining({
-                id: 7,
-                email: "michael.lawson@reqres.in",
-                first_name: "Michael",
-                last_name: "Lawson",
-                avatar: "https://reqres.in/img/faces/7-image.jpg",
-            })
-        );
-    });
-}
