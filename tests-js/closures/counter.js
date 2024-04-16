fb309f0f5f8c251090201180a5f34b53bbca36a2
Yaroslav Panchishyn <yarpan@gmail.com>
2024-04-16 23:41:26 +0300

    add new puzzles

--- tests-js/closures/counter.js
+++ tests-js/closures/counter.js
@@ -1,60 +1,36 @@
-// Source: https://leetcode.com/problems/counter-ii
-// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+// Source: https://leetcode.com/problems/counter
+// 2620. Counter
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
-// The three functions are:
-// increment() increases the current value by 1 and then returns it.
-// decrement() reduces the current value by 1 and then returns it.
-// reset() sets the current value to init and then returns it.
- 
 // Example 1:
-// Input: init = 5, calls = ["increment","reset","decrement"]
-// Output: [6,5,4]
-// Explanation:
-// const counter = createCounter(5);
-// counter.increment(); // 6
-// counter.reset(); // 5
-// counter.decrement(); // 4
+// Input: n = 10 
+// ["call","call","call"]
+// Output: [10,11,12]
+// Explanation: 
+// counter() = 10 // The first time counter() is called, it returns n.
+// counter() = 11 // Returns 1 more than the previous time.
+// counter() = 12 // Returns 1 more than the previous time.
 
 // Example 2:
-// Input: init = 0, calls = ["increment","increment","decrement","reset","reset"]
-// Output: [1,2,1,0,0]
-// Explanation:
-// const counter = createCounter(0);
-// counter.increment(); // 1
-// counter.increment(); // 2
-// counter.decrement(); // 1
-// counter.reset(); // 0
-// counter.reset(); // 0
-
+// Input: n = -2
+// ["call","call","call","call","call"]
+// Output: [-2,-1,0,1,2]
+// Explanation: counter() initially returns -2. Then increases after each sebsequent call.
 
 /**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
+ * @param {number} n
+ * @return {Function} counter
  */
-var createCounter = function(init) {
-    let count = init || 0; // initialize count to 0 or given value
-  
-    const increment = function() {
-      count++;
-      return count;
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
     };
-  
-    const decrement = function() {
-      count--;
-      return count;
-    };
-  
-    const reset = function() {
-      count = init || 0;
-      return count;
-    };
-  
-    return { increment, decrement, reset };
-  };
-  
-  /**
-   * const counter = createCounter(5)
-   * counter.increment(); // 6
-   * counter.reset(); // 5
-   * counter.decrement(); // 4
-   */
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
