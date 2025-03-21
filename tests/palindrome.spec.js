e2a8f9056dd41522bef5a77aa36e3f412724f6ac
Yaroslav Panchishyn <yarpan@gmail.com>
2025-03-21 20:57:37 +0200

    fizz

--- tests/palindrome.spec.js
+++ tests/palindrome.spec.js
@@ -32,3 +32,10 @@ function palindrome(str) {
     }
     return true;
 }
+
+function palindromeAlt(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
+}
