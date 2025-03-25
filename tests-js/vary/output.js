e39e4eb3a7cd181d72f1a60ac9d5e8bc1ad223a8
Yaroslav Panchishyn <yarpan@gmail.com>
2025-03-25 23:31:43 +0200

    output

--- tests-js/vary/output.js
+++ tests-js/vary/output.js
@@ -33,3 +33,64 @@ console.log("Кінець");
 // setTimeout
 
 // === === === === === ===
+
+console.log("Початок");
+
+setTimeout(() => {
+console.log("setTimeout 1");
+}, 0);
+
+Promise.resolve().then(() => {
+console.log("Promise 1");
+}).then(() => {
+console.log("Promise 2");
+});
+
+setTimeout(() => {
+console.log("setTimeout 2");
+}, 0);
+
+console.log("Кінець");
+
+// Початок
+// Кінець
+// Promise 1
+// Promise 2
+// setTimeout 1
+// setTimeout 2
+
+// === === === === === ===
+
+async function asyncFunc() {
+    console.log("asyncFunc початок");
+    await Promise.resolve();
+    console.log("asyncFunc кінець");
+    }
+    
+    console.log("Початок");
+    asyncFunc();
+    console.log("Кінець");
+    
+    // Початок
+    // asyncFunc початок
+    // Кінець
+    // asyncFunc кінець
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
