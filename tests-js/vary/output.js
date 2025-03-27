573513d6dc9aa8f76298582897b5f7d4c42bee9d
Yaroslav Panchishyn <yarpan@gmail.com>
2025-03-27 22:52:31 +0200

    output

--- tests-js/vary/output.js
+++ tests-js/vary/output.js
@@ -185,7 +185,164 @@ console.log("Кінець");
 
 // === === === === === ===
 
+console.log("Початок");
 
+Promise.resolve().then(() => {
+console.log("Promise 1");
+return Promise.resolve();
+}).then(() => {
+console.log("Promise 2");
+});
+
+console.log("Кінець");
+
+// Початок
+// Кінець
+// Promise 1
+// Promise 2
+
+
+// === === === === === ===
+
+console.log("Початок");
+
+setTimeout(() => {
+console.log("setTimeout");
+}, 0);
+
+async function asyncFunc() {
+console.log("asyncFunc початок");
+await new Promise(resolve => resolve());
+console.log("asyncFunc кінець");
+}
+
+asyncFunc();
+
+Promise.resolve().then(() => {
+console.log("Promise");
+});
+
+console.log("Кінець");
+
+// Початок
+// asyncFunc початок
+// Кінець
+// asyncFunc кінець
+// Promise
+// setTimeout
+
+
+// === === === === === ===
+
+async function foo() {
+console.log("foo початок");
+await bar();
+console.log("foo кінець");
+}
+
+async function bar() {
+console.log("bar початок");
+await Promise.resolve();
+console.log("bar кінець");
+}
+
+console.log("Початок");
+setTimeout(() => console.log("setTimeout"), 0);
+foo();
+console.log("Кінець");
+
+// Початок
+// foo початок
+// bar початок
+// Кінець
+// bar кінець
+// foo кінець
+// setTimeout
+
+
+// === === === === === ===
+
+async function foo() {
+    console.log("foo початок");
+    await Promise.resolve();
+    console.log("foo кінець");
+}
+
+console.log("Початок");
+foo();
+console.log("Кінець");
+
+// Початок
+// foo початок
+// Кінець
+// foo кінець
+
+
+// === === === === === ===
+
+console.log("Початок");
+
+Promise.resolve().then(() => {
+    console.log("Promise 1");
+    return Promise.resolve();
+}).then(() => {
+    console.log("Promise 2");
+});
+
+console.log("Кінець");
+
+// Початок
+// Кінець
+// Promise 1
+// Promise 2
+
+
+// === === === === === ===
+
+async function foo() {
+console.log("foo початок");
+await new Promise(resolve => setTimeout(resolve, 1000));
+console.log("foo кінець");
+}
+
+console.log("Початок");
+foo();
+console.log("Кінець");
+
+// Початок
+// foo початок
+// Кінець
+// foo кінець
+
+
+// === === === === === ===
+
+console.log("Початок");
+
+setTimeout(() => console.log("setTimeout 1"), 0);
+
+Promise.resolve().then(() => {
+console.log("Promise 1");
+setTimeout(() => console.log("setTimeout 2"), 0);
+});
+
+async function asyncFunc() {
+console.log("asyncFunc початок");
+await Promise.resolve();
+console.log("asyncFunc кінець");
+}
+
+asyncFunc();
+
+console.log("Кінець");
+
+// Початок
+// asyncFunc початок
+// Кінець
+// Promise 1
+// asyncFunc кінець
+// setTimeout 1
+// setTimeout 2
 
 
 
